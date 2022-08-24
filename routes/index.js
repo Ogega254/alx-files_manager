0ff4b3b3e3637212dc00bdbf74498167c15dca19
root <ogegagideon9@gmail.com>
2022-08-24 22:09:40 +0000

    Files

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,24 @@
-import express from 'express';
+/* eslint-disable */
+import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
-export default function Routes(app) {
-  const router = express.Router();
-  app.use('/', router);
+const router = Router();
 
-  router.get('/status', (req, res) => {
-    AppController.getStatus(req, res);
-  });
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 
-  router.get('/stats', (req, res) => {
-    AppController.getStats(req, res);
-  });
+router.post('/users', UsersController.postNew);
+router.post('/files', FilesController.postUpload);
 
-  router.post('/users', (req, res) => {
-    UsersController.postNew(req, res);
-  });
-}
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish ', FilesController.putUnpublish);
+
+export default router;
